8f3e6afd45d45a92f81365ce28242205569ccd68
Aayush Kumar Sahu <aayushmau5@gmail.com>
2025-07-05 22:23:22 +0530

    exercise-1

--- lib/main.ex
+++ lib/main.ex
@@ -2,7 +2,7 @@ defmodule Server do
   @moduledoc """
   Your implementation of a Redis server
   """
-  
+
   use Application
 
   def start(_type, _args) do
@@ -20,8 +20,8 @@ defmodule Server do
     #
     # # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # # ensures that we don't run into 'Address already in use' errors
-    # {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    # {:ok, _client} = :gen_tcp.accept(socket)
+    {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
+    {:ok, _client} = :gen_tcp.accept(socket)
   end
 end
 
