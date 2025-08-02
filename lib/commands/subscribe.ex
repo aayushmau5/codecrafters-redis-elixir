bb7a11e89ba55ecad08f41370b4c7ee62b58982a
Aayush Kumar Sahu <aayushmau5@gmail.com>
2025-08-02 16:35:35 +0530

    multiple subscriptions

--- lib/commands/subscribe.ex
+++ lib/commands/subscribe.ex
@@ -1,13 +1,18 @@
 defmodule Commands.Subscribe do
   def handle_subscribe([_, channel_name]) do
     Process.put(:in_subscription, true)
-    subscribed_channels = Process.get(:subscribed_channels, [])
-    total_channels = [channel_name | subscribed_channels]
 
-    if not already_subscribed?(channel_name),
-      do: Process.put(:subscribed_channels, total_channels)
+    if already_subscribed?(channel_name) do
+      subscribed_channels = Process.get(:subscribed_channels)
 
-    "*3\r\n$9\r\nsubscribe\r\n$#{String.length(channel_name)}\r\n#{channel_name}\r\n:#{length(total_channels)}\r\n"
+      "*3\r\n$9\r\nsubscribe\r\n$#{String.length(channel_name)}\r\n#{channel_name}\r\n:#{length(subscribed_channels)}\r\n"
+    else
+      subscribed_channels = Process.get(:subscribed_channels, [])
+      total_channels = [channel_name | subscribed_channels]
+      Process.put(:subscribed_channels, total_channels)
+
+      "*3\r\n$9\r\nsubscribe\r\n$#{String.length(channel_name)}\r\n#{channel_name}\r\n:#{length(total_channels)}\r\n"
+    end
   end
 
   defp already_subscribed?(channel_name) do
